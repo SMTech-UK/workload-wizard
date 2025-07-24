d7737f7566220c92688e5c06108a155384a98678
Sam McNab <sam@sammcnab.co.uk>
2025-07-24 15:54:46 +0100

    feat: Add vercel analytics & speed

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,17 @@
 import React from "react";
 import "./globals.css";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/next";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body>
         {children}
+        <SpeedInsights/>
+        <Analytics/>
       </body>
     </html>
+
   );
 }
